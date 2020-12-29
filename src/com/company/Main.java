2e0108a8ec69aefb0f3a9f08c6d253a0a9013607
songdalin <songdalin@qdcares.cn>
2020-12-29 14:03:52 +0800

    有向图查找闭环

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+/*        Map<String, String> map = new HashMap<>();
+        map.put("1", "11");
+        System.out.println(map.get("12"));
+        System.out.println(map.getOrDefault("12", ""));*/
+
+        String a = "";
+        Integer i = Integer.parseInt(a);
+        System.out.println(i);
     }
 }
